8771d79749127c898b3873002ed4d49b52c96e32
c-arnab <arnab.choudhuri@gmail.com>
2019-09-17 17:50:07 +0000

    first commit

---
+++ routes/index.js
@@ -0,0 +1,52 @@
+var express = require('express');
+var router = express.Router();
+const http = require('http');
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  let titletext = '';
+  let wtext = '';
+  http.get('http://mywebapi:5000/api/values', (apires) => {
+    const { statusCode } = apires;
+    const contentType = apires.headers['content-type'];
+  
+    let error;
+    if (statusCode !== 200) {
+      error = new Error('Request Failed.\n' +
+                        `Status Code: ${statusCode}`);
+    } else if (!/^application\/json/.test(contentType)) {
+      error = new Error('Invalid content-type.\n' +
+                        `Expected application/json but received ${contentType}`);
+    }
+    if (error) {
+      console.error(error.message);
+      // Consume response data to free up memory
+      apires.resume();
+      return;
+    }
+  
+    apires.setEncoding('utf8');
+    let rawData = '';
+    apires.on('data', (chunk) => { rawData += chunk; });
+    apires.on('end', () => {
+      try {
+        const parsedData = JSON.parse(rawData);
+        console.log(parsedData);
+        titletext=parsedData[1];
+        wtext=parsedData[0];
+        res.render('index', { title: titletext, data:wtext });
+      } catch (e) {
+        console.error(e.message);
+      }
+    });
+  }).on('error', (e) => {
+    console.error(`Got error: ${e.message}`);
+    res.render('index', { title: titletext, data:wtext });
+  });
+  
+
+  
+});
+
+module.exports = router;
